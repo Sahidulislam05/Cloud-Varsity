9c863a5f023c2b4c83526feb19690cdfd4e66f7c
Sahidulislam05 <sahidulislamcst@gmail.com>
2026-09-11 09:30:26 +0600

    feat:ssl commerz init

---
+++ src/types/sslcommerz-lts.d.ts
@@ -0,0 +1,15 @@
+declare module "sslcommerz-lts" {
+  export default class SSLCommerzPayment {
+    constructor(storeId: string, storePassword: string, isLive: boolean);
+    init(
+      data: Record<string, unknown>,
+    ): Promise<{
+      status: string;
+      GatewayPageURL: string;
+      [key: string]: unknown;
+    }>;
+    validate(data: {
+      val_id: string;
+    }): Promise<{ status: string; [key: string]: unknown }>;
+  }
+}
